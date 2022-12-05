5711797c749cf9acbcfcac11ddd279e89ed47e9f
Conor Hoekstra <codereport@outlook.com>
2022-12-04 21:43:59 -0500

    ch-04: initial main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::env;
+use std::io;
+use std::io::Write;
+
+fn run_prompt() {
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer).expect("Valid input");
+        if buffer.len() == 1 {
+            println!("exit");
+            break;
+        } else {
+            println!("{buffer}")
+        }
+    }
+}
+
+fn run_file(path: &String) {
+    // TODO
+    println!("{path}\n");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 2 {
+        println!("Usage: lox [script]");
+    } else if args.len() == 2 {
+        run_file(&args[1]);
+    } else {
+        run_prompt();
+    }
 }
